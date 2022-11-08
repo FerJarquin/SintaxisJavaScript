b36603167ad278cb7109c5e392a2d7d1cdc198ec
FerJarquin <114317827+FerJarquin@users.noreply.github.com>
2022-11-07 18:32:26 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,154 @@
+function condicionales(valor) {
+    if (valor.length === 0) {
+        alert("La caja de texto esta vacía")
+    } else {
+        alert("La caja de texto no esta vacía y su contenido es: " + valor)
+    }
+}
+
+function _switch(valor) {
+    switch (valor) {
+        case "Perro":
+            alert("El perro hace guau guau");
+            break;
+        case "Gato":
+            alert("El Gato hace miau");
+            break;
+        case "Vaca":
+            alert("La vaca hace mu");
+            break;
+        case "Mono":
+            alert("El mono hace Oooo Ooo Ahh Ahh");
+            break;
+        default:
+            alert("No seleccionó ningún animal");
+    }
+}
+
+function crearExcepcion(valor) {
+    if (valor === "")
+        throw new Error("No hay valor");
+}
+
+function manejarExcepcion(valor) {
+
+    try {
+        crearExcepcion(valor);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+function objetoError(error) {
+    if (error instanceof TypeError) {
+        alert("Oops, clickeaste el boton de error y esto genero un error de tipo: " + error)
+    }
+}
+
+function bucles(valor) {
+    var contador = 0;
+    switch (valor) {
+        case "for":
+            for (let i = 0; i <= 5; i++) {
+                alert(`iteracion numero ${i} con for`);
+            }
+            break;
+        case "while":
+            while (contador <= 5) {
+                alert(`iteracion numero ${contador} con while`);
+                contador++;
+            }
+            break;
+        case "do While":
+            do {
+                alert(`iteracion numero ${contador} con do while`);
+                contador++;
+            } while (contador <= 5);
+            break;
+    }
+}
+
+/*sintaxis de las funciones
+
+definir:
+function nombreFuncion(parametros o argumentos){
+
+}
+llamar:
+nombreFuncion(parametros o argumentos);
+
+tambien se puede de la siguiente forma:
+definir:
+
+const nombreFuncion = function(parasmetros o argumentos){
+
+}
+
+llamar:
+var nombreVariable = nombreFuncion(parametros o argumentos)
+*/
+
+function objetos(tipo) {
+    switch (tipo) {
+        case 'Number':
+            alert("Objeto Number: " + new Number(3).valueOf());
+            break;
+        case 'Math':
+            alert("Objeto Math: " + Math.E);
+            break;
+        case 'Date':
+            alert("Objeto Date: " + new Date().toLocaleDateString());
+            break;
+    }
+}
+
+function formatearTexto(accion, texto, textbox) {
+    switch (accion) {
+        case 'Mayúscula':
+            textbox.value = texto.toUpperCase();
+            break;
+        case 'Minúscula':
+            textbox.value = texto.toLowerCase();
+            break;
+        case 'Tamaño':
+            alert(`La cadena tiene ${texto.length} caracteres`);
+            break;
+    }
+}
+
+function operar(num1, num2, operacion) {
+    let total;
+
+    switch (operacion) {
+        case "Sumar":
+            total = Number.parseInt(num1) + Number.parseInt(num2);
+            break;
+        case "Restar":
+            total = Number.parseInt(num1) - Number.parseInt(num2);
+            break;
+        case "Multiplicar":
+            total = Number.parseInt(num1) * Number.parseInt(num2);
+            break;
+        case "Dividir":
+            total = Number.parseInt(num1) / Number.parseInt(num2);
+            break;
+    }
+    alert(`El resultado de la operacion es: ${total}`);
+}
+
+function array() {
+    let lista = new String();
+    let frutas = ["Manzana", "Banano", "Pera", "Fresa", "Uva"];
+    for (let i = 0; i < frutas.length; i++) {
+        lista += frutas[i] + "\n";
+    }
+    alert(lista)
+}
+
+function arrayTipado() {
+    const arrayTipado = new Int8Array(4);
+    for (let i = 0; i < arrayTipado.length; i++) {
+        arrayTipado[i] = i;
+    }
+    alert(arrayTipado);
+}
